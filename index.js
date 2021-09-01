1a5785bd6592c7a9a91d2f98a0e86655956073f9
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-09-01 17:36:42 +0530

    code cleaning

--- index.js
+++ index.js
@@ -32,13 +32,7 @@ switch(template) {
     templateURL = "git@github.com:Ajamuar/cra-starter.git";
     break;
 }
-// const {
-//   getStoryBookScreensCode,
-//   writeCodeToFiles,
-// } = require("./converter");
 
-// getStoryBookScreensCode();
-// writeCodeToFiles();
 exec(
   `git clone ${templateURL} native-base-starter`,
   (error, stdout, stderr) => {
